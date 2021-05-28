8dd199e569c58c88a21df8efc92345cb191fce6b
Benjamin Chai <benjaminchai99@gmail.com>
2021-05-28 14:34:56 +0800

    File upload from local

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  instagram_token: 'INSTAGRAM_TOKEN',
+  stripe_token: 'pk_test_51If6FeLGzNzKRWvvs7rKERzilpeNL5IMwkWq1nP9Z0PzE2h6PBQIKyTaieqr21kix5mzhLFg31F3DD2UX1LB7GwD00g1sC9oyr',
+  paypal_token: 'AXH2-WbBvSYQd77YL4LuZMm8MWzQor6wh49dplP4PUFIMGV5CBJuUU5w4ofc9xc8nWmP9-_BzsefypC-',
+  serverIP: 'http://localhost:3030/'
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,19 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  instagram_token: 'INSTAGRAM_TOKEN',
+  stripe_token: 'pk_test_51If6FeLGzNzKRWvvs7rKERzilpeNL5IMwkWq1nP9Z0PzE2h6PBQIKyTaieqr21kix5mzhLFg31F3DD2UX1LB7GwD00g1sC9oyr',
+  paypal_token: 'AXH2-WbBvSYQd77YL4LuZMm8MWzQor6wh49dplP4PUFIMGV5CBJuUU5w4ofc9xc8nWmP9-_BzsefypC-'
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
